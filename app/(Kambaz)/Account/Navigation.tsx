09e91a15a8ffe23594ad73941e612a07b0412c02
Sidhartha Yadav Vadlakonda <sidharthayadav@Sidharthas-MacBook-Pro.local>
2025-12-01 11:11:24 -0500

    first a6 commit

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,25 +1,43 @@
-"use client"
+"use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { ListGroup, ListGroupItem, NavLink } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+
 export default function AccountNavigation() {
-    const { currentUser } = useSelector((state: RootState) => state.accountReducer);
+  const { currentUser } = useSelector(
+    (state: RootState) => state.accountReducer
+  );
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+
   const pathname = usePathname();
- return (
-   <div id="wd-account-navigation" className="wd list-group rounded-0 fs-6">
+
+  return (
+    <div id="wd-account-navigation" className="wd list-group rounded-0 fs-6">
       {links.map((link) => (
-                <Link
-                    key={link}
-                    href={link}
-                    id={`wd-course-${link.toLowerCase()}-link`}
-                    className={`list-group-item border-0
-                        ${pathname.includes(`/${link}`) ? "active" : "text-danger"}`}
-                >
-                    {link}
-                </Link>
-            ))}
-   </div>
-);}
+        <Link
+          key={link}
+          href={`/Account/${link}`}
+          id={`wd-course-${link.toLowerCase()}-link`}
+          className={`list-group-item border-0
+              ${pathname.includes(`/${link}`) ? "active" : "text-danger"}`}
+        >
+          {link}
+        </Link>
+      ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <NavLink
+          as={Link}
+          href="/Account/Users"
+          className="list-group-item border-0"
+          active={pathname.endsWith("Users")}
+        >
+          Users
+        </NavLink>
+      )}
+
+    </div>
+  );
+}
